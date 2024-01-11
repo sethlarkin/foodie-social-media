a44e96f1a18db2cc4d3c950201e49ab11a415244
seth-consulting <sethlarkin@icloud.com>
2024-01-10 16:14:50 -0800

    adds db and meal rendering

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,13 +1,36 @@
-import Link from 'next/link' 
+import Link from 'next/link';
+import classes from './page.module.css';
+import MealsGrid from '@/components/meals/meals-grid';
+import { getMeals } from '@/lib/meals';
+import { Suspense } from 'react';
 
-
-export default function MealsPage(){
+async function Meals() {
+    const meals = await getMeals();
     return(
-        <main>
-            <h1>Meals Page</h1>
-            <h3><Link href="meals/share">Meals</Link></h3>
-            
-
-        </main>
+        <MealsGrid meals={meals} />
     )
+}
+
+export default async function MealsPage() {
+
+    return (
+    <>
+      <header className={classes.header}>
+        <h1>Delicious meals created {' '}
+        <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>Choose you favorite recepie and cook it yourself! It is easy and fun</p>
+        <p className={classes.cta}>
+            <Link href="/meals/share">
+                Share Your Favorite Recepie
+            </Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <Suspense fallback={<p className={classes.loading}>Fetching meals</p>}>
+            <Meals />
+        </Suspense>
+      </main>
+    </>
+  );
 }
