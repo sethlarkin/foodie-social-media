cdbe6a3a27109992874dad32dba1eca52d23ff6a
seth-consulting <sethlarkin@icloud.com>
2024-01-16 14:14:04 -0800

    MEAL SHARING, FORM CONTROL

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,21 +1,10 @@
 import ImagePicker from '@/components/meals/image-picker';
 import classes from './page.module.css';
 
+import { shareMeal } from '../../../lib/actions';
+import MealsFormSubmit from '@/components/meals/meals-form-submit';
+
 export default function ShareMealPage() {
-  async function shareMeal(formData){
-    'use server';
-
-    const meal = {
-      title: formData.get('title'),
-      summary: formData.get('summary'),
-      instructions: formData.get('instructions'),
-      image: formData.get('image'),
-      creator: formData.get('name'),
-      creator_email: formData.get('email'),
-    }
-
-    console.log(meal);
-  }
 
   return (
     <>
@@ -56,7 +45,7 @@ export default function ShareMealPage() {
           </p>
           <ImagePicker label="Your_image" name="image" />
           <p className={classes.actions}>
-            <button type="submit">Share Meal</button>
+            <MealsFormSubmit />
           </p>
         </form>
       </main>
