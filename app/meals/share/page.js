aaf7b46e5d1323bc24cb862c84636e2d36fb6a41
seth-consulting <sethlarkin@icloud.com>
2024-01-17 11:22:18 -0800

    form error handling

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,3 +1,7 @@
+'use client'
+
+import { useFormState } from 'react-dom';
+
 import ImagePicker from '@/components/meals/image-picker';
 import classes from './page.module.css';
 
@@ -5,6 +9,8 @@ import { shareMeal } from '../../../lib/actions';
 import MealsFormSubmit from '@/components/meals/meals-form-submit';
 
 export default function ShareMealPage() {
+
+  const [state, formAction] = useFormState(shareMeal, {message: null});
 
   return (
     <>
@@ -15,7 +21,7 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form} action={shareMeal}>
+        <form className={classes.form} action={formAction}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -44,6 +50,7 @@ export default function ShareMealPage() {
             ></textarea>
           </p>
           <ImagePicker label="Your_image" name="image" />
+          {state.message && <p>{state.message}</p>}
           <p className={classes.actions}>
             <MealsFormSubmit />
           </p>
